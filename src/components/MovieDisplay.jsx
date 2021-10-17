6d9a8d6baec64abcac70ec16b7793feeaf145566
tanpeterson <tanpeterson@gmail.com>
2021-10-16 17:06:24 -0700

    Added swipe feature to discover page

---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MovieDisplay() {
+  return (
+    <div>
+      <img src="https://i.insider.com/60817ec5354dde0018c06960?width=700" alt="" />
+      <p>Movie: Shrek</p>
+      <p>Rating: 9</p>
+    </div>
+  )
+}
