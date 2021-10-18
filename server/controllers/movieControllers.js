7134a189f0d51c0d4fb5ab63de8edcc6e1f6aef5
Jennifer Wu <jenwu1234@gmail.com>
2021-10-18 13:51:31 -0700

    midday progress

--- server/controllers/movieControllers.js
+++ server/controllers/movieControllers.js
@@ -1,19 +1,27 @@
 const db = require('../models/movieModels');
-
+const fetch = require('node-fetch')
 
 // upload to aws command:
 // \copy [table name] FROM ‘[file path]’ DELIMITER E’\t’
 
 // export controller
 module.exports = {
-  // Initial stack
-  // Get all movies (50-100)
-    getAllMovies: (req, res, next) => {
-        db.query('SELECT movies FROM movies;')
+    addSwipe: (req, res, next) => {
+        db.query(`INSERT INTO swipes (user_id, tconst, rightswipe, leftswipe) VALUES (${req.body.user}, ${req.body.imdbID}, ${req.body.rightswipe}, ${req.body.leftswipe})`)
             .then(data => {
-                // Get first 50-100, NOT all
-                console.log('data', data.slice(0,50));
-                res.locals.allMovies = data.slice(0, 50);
+                console.log('Swipe recorded');
+                return next();
+            })
+            .catch(err => {
+                console.log(err);
+                return next(err);
+            })
+    },
+
+    getRightSwipes: (req, res, next) => {
+        db.query(`SELECT tconst FROM swipes WHERE (user='${req.body.user}' AND rightswipe=true)`)
+            .then(data => {
+                res.locals.movies = data;
                 return next();
             })
             .catch(err => {
@@ -25,24 +33,51 @@ module.exports = {
     // User customized filters
     // date (year), director, actor, language, genre
     // Get filtered movies
-    getFilteredMovies: (req, res, next) => {
+    getMovies: (req, res, next) => {
        
-        // How will the request look?? 
-        // const { director, actor, date, language, genre } = req.params;
         // const { director, actor, date, language, genre } = req.body;
-
+        const { genres } = req.body;
+        const queryGenres = [];
+        for (const el in genres) { 
+            if (genres[el] === true) queryGenres.push(el);
+        }
+        const fakeGenres = ['Horror', 'Comedy'];
         // Modify this query for filtering
-        db.query('SELECT movies FROM movies;')
+        const handleGenreQuery = fakeGenres.length > 1 ? `genres IN (${genres.map(el => `'${el}'`)})` : `genres='${genres}'`
+        db.query(`SELECT tconst FROM movies WHERE ${handleGenreQuery} LIMIT 50;`)
             .then(data => {
-                // Get first 50-100, NOT all
-                console.log('data', data.slice(0,50));
-                res.locals.filteredMovies = data.slice(0, 50);
+                res.locals.movies = data.rows;
+                console.log('Query returned successfully');
                 return next();
             })
             .catch(err => {
                 console.log(err);
                 return next(err);
             })
-    }
+    },
 
+    getOMDBInfo: async (req, res, next) => {
+        const apiKey = 'da899953';
+        const newMovies = [];
+        for (const each of res.locals.movies) {
+            const imdb_id = each.tconst;
+            await fetch(`http://www.omdbapi.com/?i=${imdb_id}&apiKey=${apiKey}`)
+                .then(data => data.json())
+                .then(response => {
+                    if ('Poster' in response && response['Poster'] !== 'N/A') {
+                        each.posterUrl = response['Poster'];
+                        each.director = response['Director'];
+                        each.plot = response['Plot'];
+                        newMovies.push(each); 
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                    return next(error);
+                })
+        }
+        res.locals.movies = newMovies;
+        console.log('length of res.locals.movies', res.locals.movies.length)
+        return next();
+    }
 }
--- server/models/movieModels.js
+++ server/models/movieModels.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
-const URI = 'psql --host=scratch-db.cvmmondruc9o.us-east-2.rds.amazonaws.com --port=5432 --username=wonderpus --password --dbname=postgres';
-// test if we need to submit pw when submitting queries/getting data
-
 
 const pool = new Pool({
-    connectionString: URI
+  user: 'wonderpus',
+  host: 'scratch-db.cvmmondruc9o.us-east-2.rds.amazonaws.com',
+  database: 'postgres',
+  password: 'photogenicus',
+  port: 5432,
 });
 
 module.exports = {
